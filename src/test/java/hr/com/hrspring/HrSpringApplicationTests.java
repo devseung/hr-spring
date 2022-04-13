93060737bb705d5c386782b14c4fe523db777c42
Seung <dev_seung@naver.com>
2022-04-13 22:57:11 +0900

    Initial commit

---
+++ src/test/java/hr/com/hrspring/HrSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.com.hrspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
